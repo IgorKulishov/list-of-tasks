42cc4e513996ca9350bf342bacc0380857337a22
Igor Kulishov <igorkulishov@gmail.com>
2015-03-29 01:08:36 -0400

    PS-57 working programm need to test

--- public/js/script.js
+++ public/js/script.js
@@ -5,63 +5,53 @@ angular.module('listTask', [])
     this.name = "New task..";
     this.priority = 1;
     this.type = "read&study";
-    this.progress;
-    this.progress_print = "not started";
+    this.progress = false;
+    this.message = "  Click to start the task ";
     
+    
+//to add new Task
     this.save = function() {
         
-        this.array.push({number: this.number, name: this.name, priority: this.priority, type: this.type, progress: this.progress});
-        addressJson.data = this.array;
+        this.array.push({number: this.number, name: this.name, priority: this.priority, 
+            type: this.type, progress: this.progress, message: this.message});
+        addressJson.backup = this.array;
         this.number += 1;
     };
 
 })
-.controller("ProgressController", function() {
-    this.progress_info = "not started";
-    this.progress_changed = function() {
-        if (this.progress == true) {
-            this.progress_info = "started";
-        } 
-
-        if (this.progress == false) {
-            this.progress_info = "not started";
+//to indicate Progress in a Task when clicked
+.controller("infoProgress", function(addressJson) {
+    var n = 0;
+    this.number = 0;
+    this.click = function() {
+        if ((this.progress == true) && (n == 0)) { 
+            addressJson.backup[(this.number - 1)].message = "  started " + this.number;
+            n += 1;
         }
+
+        if ((this.progress == false) && (n == 1)) { 
+            addressJson.backup[(this.number - 1)].message = "  postponed " + this.number;
+            n += 1; 
+        }
+
+        if ((this.progress == true) && (n == 2)) { 
+            addressJson.backup[(this.number - 1)].message = "  closed " + this.number;
+            n += 1;
+        }
+
+        if ((this.progress == false) && (n==3)) { 
+            addressJson.backup[(this.number - 1)].message = "  Click to start the task " + this.number;
+            n += 1;
+        }             
+
+            
+        if (n == 4) {
+            n = 0;
+        }
+      
     };
 })
-.controller("EditController", function(testService) {
-    this.edit = function() {
-        
-        if (this.check == true) {
-            testService.checked();
-        }
-        
-        if (this.check == false) {
-            testService.unchecked();
-        }
-    };
-})
-.factory("testService", function(addressJson) {
-    
-        return {
-            checked : function() {
-                var message;
-                for (var i = 0; i < addressJson.data.length; i++) {
-//need to pass 
-                    if (addressJson.data[i].progress != true) {
-                        message = 'progress of ' + alert('checked: ' + addressJson.data[i].name + ' was changed');
-                    
-                        alert('checked: ' + message);
-                    }
-                }
-
-                
-            },
-            unchecked : function() {
-                alert('unchecked');
-            },
-        
-        }
-})
+//service keeps copy of JSON
 .factory("addressJson", function() {
-    return data = [];
+    return backup = [];
 });
