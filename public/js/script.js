8ca84abf8faa5d2b8b4ae8b10c7ea88e2a0eb3a4
Igor Kulishov <igorkulishov@gmail.com>
2015-03-31 18:45:57 -0400

    PS-57 fix v.2

--- public/js/script.js
+++ public/js/script.js
@@ -6,16 +6,21 @@ angular.module('listOfTasks', [])
         this.priority = 1;
         this.type = "boolean";
         this.progress = false;
-        this.message = "  Click to start the task ";        
+        this.progressMessage = "  Click to start the task ";        
+        this.numDelete;
 //function to add new Task
         this.submit = function() {            
             this.array.push({
                 number: this.number, name: this.name, priority: this.priority, 
-                type: this.type, progress: this.progress, message: this.message
+                type: this.type, progress: this.progress, progressMessage: this.progressMessage
             });
             addressJson.backup = this.array;
             this.number += 1;
         };
+
+        this.delete = function() {
+            this.array.splice(this.numDelete, 1);
+        };        
     })
 //to indicate Progress in a Task when clicked [v] button in user's menu to change Progress of a Task
     .controller("infoProgress", function(addressJson) {
@@ -23,19 +28,19 @@ angular.module('listOfTasks', [])
         this.number = 0;
         this.click = function() {
             if ((this.progress === true) && (n === 0)) { 
-                addressJson.backup[(this.number - 1)].message = "  started " + this.number;
+                addressJson.backup[(this.number - 1)].progressMessage = "  started " + this.number;
                 n += 1;
             }
             if ((this.progress === false) && (n === 1)) { 
-                addressJson.backup[(this.number - 1)].message = "  postponed " + this.number;
+                addressJson.backup[(this.number - 1)].progressMessage = "  postponed " + this.number;
                 n += 1; 
             }
             if ((this.progress === true) && (n === 2)) { 
-                addressJson.backup[(this.number - 1)].message = "  closed " + this.number;
+                addressJson.backup[(this.number - 1)].progressMessage = "  closed " + this.number;
                 n += 1;
             }
             if ((this.progress === false) && (n===3)) { 
-                addressJson.backup[(this.number - 1)].message = "  Click to start the task " + this.number;
+                addressJson.backup[(this.number - 1)].progressMessage = "  Click to start the task " + this.number;
                 n += 1;
             }                
             if (n === 4) {
