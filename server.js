62341b1156d5134e50a9819f34bc9931409226af
Igor Kulishov <igorkulishov@gmail.com>
2015-05-13 18:57:51 -0400

    PS-63 server w GET method

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-var express = require("express");
+var express = require('express'),
+    fs = require('fs'),
+    util = require('util'),
+    bodyParser = require('body-parser'),
+    app = express(); 
+    var idGenerator = 0;
+    var todoData;
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+// to support URL-encoded bodies
+app.use(bodyParser.urlencoded({     
+  extended: true
+})); 
 
 app.use(function(req, res){
     res.type('text/plain');
@@ -15,6 +27,43 @@ app.use(function(err, req, res, next){
     res.type('text/plain');
     res.status(500);
     res.send('500 - Server Error');
+});
+
+todoData = [
+  {
+    "id":1,
+    "name":"Task 1",
+    "priority":1,
+    "type":"boolean",
+    "booleanProgress":true,
+    "percentageMessage":0,
+    "isEditing":false
+  },
+  {
+    "id":2,
+    "name":"Task 2",
+    "priority":1,
+    "type":"boolean",
+    "booleanProgress":true,
+    "percentageMessage":0,
+    "isEditing":false
+  },
+  {
+    "id":3,
+    "name":"Task 3",
+    "priority":1,
+    "type":"boolean",
+    "booleanProgress":true,
+    "percentageMessage":0,
+    "isEditing":false
+  }
+];
+
+app.get('/rest/todo', function(req, res, next){
+    //res.send([{"id":0,"name":"first task","priority":1,"type":"boolean","booleanProgress":false,"percentageMessage":0,"isEditing":false},{"id":1,"name":"test last last","priority":"2","type":"boolean","booleanProgress":false,"percentageMessage":0,"isEditing":false},{"id":1,"name":"test last last","priority":"2","type":"boolean","booleanProgress":false,"percentageMessage":0,"isEditing":false}]);
+    console.log(taskArray);
+    res.send(taskArray);           
+    next();
 });
 
 app.listen(app.get('port'), function() {
