7089583777e9fdc7579784f6e974ba866ec138f0
Igor Kulishov <igorkulishov@gmail.com>
2015-05-14 00:16:19 -0400

    PS-65 server GET

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var app = express();
-var todoData;
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(), 
+    idGenerator = 0,
+    todoData;
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+// to support URL-encoded bodies
+app.use(bodyParser.urlencoded({     
+  extended: true
+})); 
 
 todoData = [
   {
@@ -36,8 +44,9 @@ todoData = [
 ];
 //send list of tasks to a client
 app.get('/rest/todo', function(req, res, next) {
+    //res.send([{"id":0,"name":"first task","priority":1,"type":"boolean","booleanProgress":false,"percentageMessage":0,"isEditing":false},{"id":1,"name":"test last last","priority":"2","type":"boolean","booleanProgress":false,"percentageMessage":0,"isEditing":false},{"id":1,"name":"test last last","priority":"2","type":"boolean","booleanProgress":false,"percentageMessage":0,"isEditing":false}]);
     console.log(todoData);
-    res.send(todoData);        
+    res.send(todoData);            
 });
 //add new task from a client
 app.post('/rest/todo', function(req, res, next){    
