7ecf8eb5e809a1c0e9be8e1f23d8e8e2167729c5
Igor Kulishov <igorkulishov@gmail.com>
2015-06-05 17:26:35 -0400

    PS-72 removed non- methods, GET method returns promise

--- public/js/service.js
+++ public/js/service.js
@@ -1,32 +1,19 @@
 function jsonService($http) {
-    //array of tasks in service 
-    var list = [];
-    $http.get('rest/todo.json').then(function(response) {
-        list = response.data;
-    });
+    $http.get('rest/todo.json').then();
     //return array to controller and than to "todoListController" in html
     return {readList: function() {
-            return list;                
             }, addNewTask: function(newTask) {
-                list.push(newTask);                    
-            }, deleteTask: function(deleteTaskNumber) {
-                for (var i = 0; i < list.length; i++) {
-                    if (list[i].id === deleteTaskNumber)
-                        list.splice(i, 1);
-                }
-            }, editTask: function(editTaskNumber) {
-                for (var i = 0; i < list.length; i++) {
-                    if (list[i].id === editTaskNumber)
-                        list[i].isEditing = true; 
-                }
-            }, saveTask: function(saveTaskNumber) {
-                for (var i = 0; i < list.length; i++) {
-                    if (list[i].id === saveTaskNumber)
-                        list[i].isEditing = false; 
-                }                
-            }, changeProgressInfo: function(changeTaskNumber) {
-                //jreserved function to track clicks
                 
+                                   
+            }, deleteTask: function(deleteTaskNumber) {
+                
+                
+            }, editTask: function(editTaskNumber) {
+                
+                
+            }, saveTask: function(saveTaskNumber) {
+                
+                                
             }
     };
 };
